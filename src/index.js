c9f4cc634d4e8a8a9388f2daef66184905bfa4c7
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2024-01-24 18:25:12 +0200

    added express handlebars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
+//declaring express and handlebars
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const port = 5000;
 
+
+app.engine('hbs', handlebars.engine({
+  extname: 'hbs',
+}));
+
+// view engine
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+// Home page 
 app.get('/', (req, res) => {
-  res.send('Home Page');
+  res.render('home', {layout: false});
 });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}...`));
