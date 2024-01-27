c7a4402103f41875c4ef69b5fd22684cf7adc00e
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2024-01-27 17:18:20 +0200

    added about page

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -2,7 +2,11 @@ const router = require('express').Router();
 
 // Home page 
 router.get('/', (req, res) => {
-  res.render('home',);
+  res.render('home');
+});
+
+router.get('/about', (req, res) => {
+  res.render('about');
 });
 
 module.exports = router;
