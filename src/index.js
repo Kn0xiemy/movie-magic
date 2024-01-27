365c22cfca51ef9602191486747326150d0069e4
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2024-01-27 16:47:08 +0200

    setup home page

--- src/index.js
+++ src/index.js
@@ -15,6 +15,8 @@ app.engine('hbs', handlebars.engine({
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 // Home page 
 app.get('/', (req, res) => {
