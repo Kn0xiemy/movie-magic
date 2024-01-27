74de85f851e36765cc584c3aa94babf2e6bada65
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2024-01-27 17:27:03 +0200

    added create page

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('./controllers/homeController');
+const movieController = require('./controllers/movieController');
 
 router.use(homeController);
+router.use(movieController);
 
 module.exports = router;
