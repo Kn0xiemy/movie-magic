11ea2296148c77c67530a388d505050a4b578137
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2024-01-30 19:56:39 +0200

    added movie id

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,7 +1,10 @@
-const movies =[];
+const movies =[{
+  _id: '1',
+}];
 exports.getAll = () => {
   return movies.slice();
 }
 exports.create = (movieData) => {
+  movieData._id = movies[movies.length - 1]._id + 1;
   movies.push(movieData);
 }
