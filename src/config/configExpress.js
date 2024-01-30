b2327bcfe6290deafbd50ac67d199c37e96ec5bb
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2024-01-30 19:26:45 +0200

    added movie service

--- src/config/configExpress.js
+++ src/config/configExpress.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
 const { config } = require('process');
+const movieService = require('../services/movieService');
 
 function configExpress(app) {
   app.use(express.static(path.resolve('src/public')));
+  app.use(express.urlencoded({extended: false}))
 }
 
 
--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -4,4 +4,10 @@ router.get('/create', (req, res) => {
   res.render('create');
 });
 
+router.post('/create', (req, res) => {
+  const newMovie = req.body;
+  movieService.create(newMovie)
+  res.send('movie should be created')
+})
+
 module.exports = router;
