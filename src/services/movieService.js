9fcfcabc0e49feface7de760f5e138b14d377c13
Kn0xiemy <112770389+Kn0xiemy@users.noreply.github.com>
2024-01-30 20:42:57 +0200

    search page

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -5,6 +5,21 @@ exports.getAll = () => {
   return movies.slice();
 }
 
+exports.search = (title, genre, year) => {
+  let movies = movies.slice();
+  if (title) {
+    movies = movies.filter(movie => movies.title.includes(title));
+  }
+  if (genre) {
+    movies = movies.filter(movie => movies.genre === genre);
+  }
+  if (year) {
+    movies = movies.filter(movie => movies.year === genre);
+  }
+
+  return movies;
+}
+
 exports.getOne = (movieId) => {
   return movies.find(movie => movie._id == movieId);
 
